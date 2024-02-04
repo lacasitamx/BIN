2b887730b48379f1f19900ee11097909a0985c8f
ChumoGH <57652264+ChumoGH@users.noreply.github.com>
2024-02-04 18:25:07 -0500

    Update RUsersAUTO.sh

--- Plugins/Extras/RUsersAUTO.sh
+++ Plugins/Extras/RUsersAUTO.sh
@@ -47,14 +47,12 @@ subironline(){
 chmod -R 755 /var/www
 cp $HOME/$arquivo_move /var/www/$arquivo_move
 cp $HOME/$arquivo_move /var/www/html/$arquivo_move
-service apache2 restart
 #portFTP=$(lsof -V -i tcp -P -n | grep apache2 | grep -v "ESTABLISHED" |grep -v "COMMAND" | grep "LISTEN" | cut -d: -f2 | cut -d' ' -f1 | uniq)
 local portFTP=$(echo -e "$_SFTP" |cut -d: -f2 | cut -d' ' -f1 | uniq)
 local portFTP=$(echo ${portFTP} | sed 's/\s\+/,/g' | cut -d , -f1)
 #_pFTP="$(lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND" | grep "LISTEN" | grep apache2 | cut -d " " -f1 | uniq)"
 [[ -z ${_SFTP} ]] && echo -e "SERVICIO FTP NO ACTIVO " || {
-IP="$(fun_ip)"
-echo -e "\033[1;36m http://$IP:${portFTP}/$arquivo_move\033[0m"
+echo -e "\033[1;36m http://$(cat /bin/ejecutar/IPcgh):${portFTP}/$arquivo_move\033[0m"
 echo -e "$barra"
 echo -e "${cor[5]}Carga Exitosa!"
 echo -e "$barra"
@@ -70,8 +68,7 @@ echo -e "\033[1;37mHaciendo Backup de Usuarios...\033[0m"
 #for user in `awk -F : '$3 > 900 { print $1 }' /etc/passwd |grep -v "nobody" |grep -vi polkitd |grep -vi systemd-[a-z] |grep -vi systemd-[0-9] |sort`
 for user in `cat "/etc/passwd"|grep 'home'|grep 'false'|grep -v 'syslog' | cut -d: -f1 |sort`
 do
-if [ -e $dir$dir_user/$user ]
-then
+[[ -e $dir$dir_user/$user ]] && {
 pass=$(cat $dir$dir_user/$user | grep "senha" | awk '{print $2}')
 limite=$(cat $dir$dir_user/$user | grep "limite" | awk '{print $2}')
 data=$(cat $dir$dir_user/$user | grep "data" | awk '{print $2}')
@@ -87,15 +84,32 @@ fi
 sl=$((dias_use + 1))
 i=$((i + 1))
 [[ -z "$limite" ]] && limite="5"
-else
+} || {
+linea=$(cat /etc/passwd | grep -w ${user})
+if [[ "${linea}" =~ ,([^:]+): ]]; then
+        pass="${BASH_REMATCH[1]}"
+fi
+limite="$(cat /etc/passwd | grep -w ${user} | awk -F ':' '{split($5, a, ","); print a[1]}')"
+data_sec=$(date +%s)
+data_user=$(chage -l "$user" |grep -i co |awk -F ":" '{print $2}')
+data_user_sec=$(date +%s --date="$data_user")
+variavel_soma=$(($data_user_sec - $data_sec))
+dias_use=$(($variavel_soma / 86400))
+if [[ "$dias_use" -le 0 ]]; 
+then
+dias_use=0
+fi
+sl=$((dias_use + 1))
+i=$((i + 1))
+[[ -z "$limite" ]] && limite="5"
 echo -e "\033[1;31mNo fue posible obtener la contraseña del usuario\033[1;37m ($user)"
 #read -p "Introduzca la contraseña manualmente o pulse ENTER: " pass
  if [ -z "$pass" ]; then
 pass="$user"
  fi
-fi
-[[ $(echo $limite) = "HWID" ]] && echo "$user:$user:HWID:$sl:$pass" >> $bc && echo -e "\033[1;37mUser $pass \033[0;35m [\033[0;36m$limite\033[0;35m]\033[0;31m Backup [\033[1;31mOK\033[1;37m] con $sl DIAS\033[0m"
-[[ $(echo $limite) = "TOKEN" ]] && echo "$user:$passTK:TOKEN:$sl:$pass" >> $bc && echo -e "\033[1;37mUser $pass \033[0;35m [\033[0;36m$limite\033[0;35m]\033[0;31m Backup [\033[1;31mOK\033[1;37m] con $sl DIAS\033[0m"
+}
+[[ ${limite} = "HWID" ]] && echo "$user:$user:HWID:$sl:$pass" >> $bc && echo -e "\033[1;37mUser $pass \033[0;35m [\033[0;36m$limite\033[0;35m]\033[0;31m Backup [\033[1;31mOK\033[1;37m] con $sl DIAS\033[0m"
+[[ ${limite} = "TOKEN" ]] && echo "$user:$passTK:TOKEN:$sl:$pass" >> $bc && echo -e "\033[1;37mUser $pass \033[0;35m [\033[0;36m$limite\033[0;35m]\033[0;31m Backup [\033[1;31mOK\033[1;37m] con $sl DIAS\033[0m"
 [[ "$limite" =~ ^[0-9]+$ ]] && echo "$user:$pass:$limite:$sl" >> $bc && echo -e "\033[1;37mUser $user \033[0;35m [\033[0;36mSSH\033[0;35m]\033[0;31m Backup [\033[1;31mOK\033[1;37m] con $sl DIAS\033[0m"
 done
 echo " "
